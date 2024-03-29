3ccd2681c7c6b916492b06b839bb39abb8213405
unknown <ankitbasu14@gmail.com>
2024-03-30 00:29:06 +0530

    configured checks.yml and docker-compose.yml files

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,21 @@
 from django.core.management.base import BaseCommand
 
+from psycopg2 import OperationalError as psycopg2Error
+from django.db.utils import OperationalError
+import time
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('waiting for db...')
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (psycopg2Error, OperationalError):
+                self.stdout.write('database unavailable, waiting 1 sec')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('database available'))
